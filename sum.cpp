3accd2ae029f632059703e0dc59fc9242f9dc848
Vansh-jadeja <73414858+Vansh-jadeja@users.noreply.github.com>
2021-01-16 21:19:33 -0800

    cpp-lesson-1

---
+++ sum.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int add(int a, int b) 
+{
+    int sum = a + b;
+    return sum;
+}
+
+int main()
+{
+    cout << "Hello, Vansh: " ;
+    int a1 = 5;
+    int b1 = 6;
+    int sum1 = add(a1, b1);
+    cout << sum1;
+    cout << ". ";
+    
+    
+    int sum2 = add(7, 6);
+    cout << sum2;
+    cout << ". ";
+
+    cout << "Bye" ;
+    return 0;
+}
